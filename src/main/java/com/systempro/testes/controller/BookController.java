c63fb9c7693c30fe393cb7ea9a14b6ad5019d3b8
Fernandolilo <fernando.sistenpro@outlook.com>
2023-10-27 13:18:23 -0300

    limpado o BookController

--- src/main/java/com/systempro/testes/controller/BookController.java
+++ src/main/java/com/systempro/testes/controller/BookController.java
@@ -2,10 +2,7 @@ package com.systempro.testes.controller;
 
 import com.systempro.testes.domain.dto.BookDTO;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/books")
@@ -13,13 +10,7 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDTO create() {
-
-        BookDTO dto = new BookDTO();
-        dto.setAutor("Fernando");
-        dto.setTitle("Meu Livro");
-        dto.setIsbn("123456");
-        dto.setId(1L);
+    public BookDTO create(@RequestBody BookDTO dto) {
         return dto;
     }
 }
